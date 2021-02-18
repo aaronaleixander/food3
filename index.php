f2579b1832df9c34566507708670d0ef83a5963a
Aaron Utterback <Autterback3@mail.greenriver.edu>
2021-02-18 14:31:32 -0500

    added sticky forms

--- index.php
+++ index.php
@@ -1,90 +1,131 @@
 <?php
-// CONTROLLER
-// turn on error reporting
-ini_set('display_error', 1);
+/** Create a food order form */
+
+//Turn on error reporting
+ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-// Start a session
+//Start a session
 session_start();
 
-// require the autoload files
+//Require files
 require_once('vendor/autoload.php');
 require_once('model/data-layer.php');
-require_once('model/validate.php');
 
-// create an instance of the base class
+
+//Instantiate my classes.
 $f3 = Base::instance();
+$validator = new Validate();
 
-// Define a default route
-$f3->route('GET /' , function(){
-    // fat free - taking the view page and rendering it in the browser
-    $view = new Template();
-    echo $view->render('/views/home.html');
-});
+//Turn on Fat-Free error reporting
+$f3->set('DEBUG', 3);
 
-// Define an order route
-$f3->route('GET|POST /order' , function($f3){
-
-
-    // if the form has been submitted - validation
-    if($_SERVER['REQUEST_METHOD'] == 'POST'){
-
-        // get the data from the post array
-        $userFood = trim($_POST['food']);
-        $userMeal = trim($_POST['meal']);
-
-        // if the data is valid - store in the session
-        if(validFood($userFood)){
-            $_SESSION['food'] = $userFood;
-        } else { // if the data is not valid -> set an error in F3 hive
-            $f3->set('errors["food"]', "Food cannot be blank and must not contain numbers");
-        }
-
-        if(isset($_POST['meal'])){
-            $_SESSION['meal'] = $_POST['meal'];
-        }
-
-        // if no errors redirect to order 2 route --- GET|POST is essential
-        if(empty($f3->get('errors'))){
-            $f3->reroute('/order2');
-        }
-    }
-
-    $f3->set('meals', getMeals());
-    $f3->set('userFood', isset($userFood) ? ($userFood) : "");
-
-    $view = new Template();
-    echo $view->render('/views/form1.html');
-});
-
-// Define an order2 route
-$f3->route('GET|POST /order2' , function($f3){
-    // HIVE
-    $f3->set('condiments', getCondiments());
-
-    //var_dump($_POST); // first page getting data
-
+//Define a default route
+$f3->route('GET /', function() {
 
     //Display a view
     $view = new Template();
-    echo $view->render('/views/form2.html');
+    echo $view->render('views/home.html');
 });
 
-// Define an order route
-$f3->route('POST /summary' , function(){
+//Define an order route
+$f3->route('GET|POST /order', function($f3) {
+
+    //var_dump($_POST);
+    global $validator;
+
+    //If the form has been submitted
+    if ($_SERVER['REQUEST_METHOD']=='POST') {
+
+        //Get the data from the POST array
+        $userFood = trim($_POST['food']);
+        $userMeal = $_POST['meal'];
+
+        //If the data is valid --> Store in session
+        if($validator->validFood($userFood)) {
+            $_SESSION['food'] = $userFood;
+        }
+        //Data is not valid -> Set an error in F3 hive
+        else {
+            $f3->set('errors["food"]', "Food cannot be blank and must contain only characters");
+        }
+
+        if($validator->validMeal($userMeal)) {
+            $_SESSION['meal'] = $userMeal;
+        }
+        else {
+            $f3->set('errors["meal"]', "Select a meal");
+        }
+
+        //If there are no errors, redirect to /order2
+        if(empty($f3->get('errors'))) {
+            $f3->reroute('/order2');  //GET
+        }
+    }
+
+    //var_dump($_POST);
+    $f3->set('meals', getMeals());
+    $f3->set('userFood', isset($userFood) ? $userFood : "");
+    $f3->set('userMeal', isset($userMeal) ? $userMeal : "");
+
+    //Display a view
+    $view = new Template();
+    echo $view->render('views/form1.html');
+});
+
+//Define an order2 route
+$f3->route('GET|POST /order2', function($f3) {
+    global $validator;
+    //If the form has been submitted
+    if ($_SERVER['REQUEST_METHOD']=='POST') {
+
+        //If condiments were selected
+        if(isset($_POST['conds'])) {
+
+            //Get condiments from post array
+            $userCondiments = $_POST['conds'];
+
+            //Data is valid -> Add to session
+            if ($validator->validCondiments($userCondiments)) {
+                $_SESSION['conds'] = implode(", ", $userCondiments);
+            }
+            //Data is not valid -> We've been spoofed!
+            else {
+                $f3->set('errors["conds"]', "Go away, evildoer!");
+            }
+        }
+
+        //If there are no errors, redirect user to summary page
+        if (empty($f3->get('errors'))) {
+            $f3->reroute('/summary');
+        }
+    }
+
+    $f3->set('condiments', getCondiments());
+
+    //Display a view
+    $view = new Template();
+    echo $view->render('views/form2.html');
+});
+
+//Define a summary route
+$f3->route('GET /summary', function() {
+
     //echo "<p>POST:</p>";
-    var_dump($_POST);
+    //var_dump($_POST);
 
     //echo "<p>SESSION:</p>";
     //var_dump($_SESSION);
 
-    if(isset($_POST['conds'])){
-        $_SESSION['conds'] = implode(", ",$_POST['conds']);
-    }
-
+    //Display a view
     $view = new Template();
-    echo $view->render('/views/summary.html');
+    echo $view->render('views/summary.html');
+
+    //Write to database
+
+    //Clear the SESSION array
+    session_destroy();
 });
 
-// Run fat free
+//Run Fat-Free
 $f3->run();
--- model/validate.php
+++ model/validate.php
@@ -3,10 +3,39 @@
  * Contains validation for food application
  */
 
-/** validFood() returns true if is not empty and contains only letters */
-function validFood($food){
-    // $validFoods = array("a", "b", "c");
+
+// creating a validate class for our validation methods
+class Validate
+{
+    /** validFood() returns true if is not empty and contains only letters */
+    function validFood($food)
+    {
+        // $validFoods = array("a", "b", "c");
 
 
-    return !empty($food) && ctype_alpha($food);
+        return !empty($food) && ctype_alpha($food);
+    }
+
+    /**
+     * ValidMeal() returns true if the selected meal is in the list of valid options
+     *
+     */
+    function validMeal($meal)
+    {
+        return in_array($meal, getMeals());
+    }
+
+    /**
+     * validCondiments() returns true of all selected condiments are valid
+     */
+    function validCondiments($selectedConds)
+    {
+        $validConds = getCondiments();
+        foreach ($selectedConds as $selected) {
+            if (!in_array($selected, $validConds)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
